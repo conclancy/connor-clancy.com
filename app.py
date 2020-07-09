4ed7cd8cbd3fc32c66060499cb9130cd3aac2bf5
connor <conclancy@hotmail.com>
2020-07-08 21:41:48 -0400

    Added icons and resume to navbar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -14,6 +14,11 @@ def experience():
 def education():
     return render_template('education.html')
 
+@app.route('/resume')
+def resume():
+    return redirect('./static/resume.pdf')
+    # https://stackoverflow.com/questions/18281433/flask-handling-a-pdf-as-its-own-page
+    
 if __name__ == '__main__':
     app.debug = True
     app.run()
